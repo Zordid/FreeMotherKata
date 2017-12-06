8c7a2e60ae6881b3bdea7a62bc899de016767b70
Olaf Gottschalk <Olaf.Gottschalk@bmwgroup.com>
2017-12-06 20:32:10 +0100

    Third test green

--- src/main/RescueMotherKotlin.kt
+++ src/main/RescueMotherKotlin.kt
@@ -5,8 +5,15 @@ class NumberCracker {
          * As per code definition, condition 1 means:
          * - must be 9 different digits
          */
-        fun testCondition1(str:String): Boolean {
+        fun testCondition1(str: String): Boolean {
             return str.toSet().size == 9
         }
+
+        fun testCondition2(str: String): Boolean {
+            val reversed = str.reversed().toInt()
+            val sqrt = Math.sqrt(reversed.toDouble());
+            return sqrt.toInt() * sqrt.toInt() == reversed
+        }
+
     }
 }
