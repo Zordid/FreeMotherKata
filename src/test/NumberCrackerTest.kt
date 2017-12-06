643d97fd309beff14ff195bd84ff8b4488a8c865
Olaf Gottschalk <Olaf.Gottschalk@bmwgroup.com>
2017-12-06 20:17:28 +0100

    Second test fails

--- src/test/NumberCrackerTest.kt
+++ src/test/NumberCrackerTest.kt
@@ -6,7 +6,14 @@ class NumberCrackerTest {
 
     @Test
     fun `Teste Bedingung 1 nur auf Länge`() {
-        Assert.assertFalse(NumberCracker.testCondition1("123"));
+        Assert.assertFalse(NumberCracker.testCondition1("123"))
+        Assert.assertTrue(NumberCracker.testCondition1("123456789"))
+    }
+
+    @Test
+    fun `Teste Bedingung 1 Länge und unterschiedliche Ziffern`() {
+        Assert.assertTrue(NumberCracker.testCondition1("123456789"))
+        Assert.assertFalse(NumberCracker.testCondition1("123456788"))
     }
 
 }
