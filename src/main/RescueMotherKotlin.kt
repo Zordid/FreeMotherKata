5d50e5428c3d77b9a1f6bf110d9c0afa8b1b1112
Olaf Gottschalk <Olaf.Gottschalk@bmwgroup.com>
2017-12-06 20:25:51 +0100

    Third test fails

--- src/main/RescueMotherKotlin.kt
+++ src/main/RescueMotherKotlin.kt
@@ -1,5 +1,10 @@
 class NumberCracker {
     companion object {
+
+        /**
+         * As per code definition, condition 1 means:
+         * - must be 9 different digits
+         */
         fun testCondition1(str:String): Boolean {
             return str.toSet().size == 9
         }
--- src/test/NumberCrackerTest.kt
+++ src/test/NumberCrackerTest.kt
@@ -8,12 +8,20 @@ class NumberCrackerTest {
     fun `Teste Bedingung 1 nur auf Länge`() {
         Assert.assertFalse(NumberCracker.testCondition1("123"))
         Assert.assertTrue(NumberCracker.testCondition1("123456789"))
+        Assert.assertFalse(NumberCracker.testCondition1("1234567890"))
     }
 
     @Test
     fun `Teste Bedingung 1 Länge und unterschiedliche Ziffern`() {
         Assert.assertTrue(NumberCracker.testCondition1("123456789"))
+        Assert.assertTrue(NumberCracker.testCondition1("123456798"))
         Assert.assertFalse(NumberCracker.testCondition1("123456788"))
+    }
+
+    @Test
+    fun `Teste Bedingung 2`() {
+        Assert.assertTrue(NumberCracker.testCondition2("46"))
+        Assert.assertFalse(NumberCracker.testCondition2("64"))
     }
 
 }
